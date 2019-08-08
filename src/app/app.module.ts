d677a6a6256e002c907658b1e84aaf19865ca0d0
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-08 10:22:01 +0100

    basket.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule  } from './app-material.module';
 import { FlexModule } from '@angular/flex-layout';
+import { BasketComponent } from './basket/basket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
