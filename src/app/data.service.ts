e4279f1fde4f0db0e3669059b1560bd55f329c1c
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-08 10:46:05 +0100

    baskets logic

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,5 +5,44 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  constructor() { }
+  basket = [
+    { 
+      name: "Peas", 
+      price: "0.95", 
+      type: "bag",
+      quantity: 0 
+    }, 
+    { 
+      name: "Eggs", 
+      price: "2.10", 
+      type: "dozen",
+      quantity: 0 
+    },
+    { 
+      name: "Milk",  
+      price: "1.30", 
+      type: "bottle",
+      quantity: 0  
+    },
+    {
+      name: "Beans", 
+      price: "0.73", 
+      type: "can",
+      quantity: 0 
+    }
+  ]
+
+  constructor() { } 
+
+  addProduct(id){
+    this.basket[id]["quantity"] += 1;
+  }
+
+  addDelete(id){
+    this.basket[id]["quantity"] -= 1;
+  }
+
+  getBasketContents(){ 
+    return this.basket;
+  }
 }
