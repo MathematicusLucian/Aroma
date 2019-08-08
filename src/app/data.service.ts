3558668b6f658a86d4dad6b2f367a8e7fcf8ab3e
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-08 11:13:20 +0100

    getTotal

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -8,25 +8,25 @@ export class DataService {
   basket = [
     { 
       name: "Peas", 
-      price: "0.95", 
+      price: 0.95, 
       type: "bag",
       quantity: 0 
     }, 
     { 
       name: "Eggs", 
-      price: "2.10", 
+      price: 2.10, 
       type: "dozen",
       quantity: 0 
     },
     { 
       name: "Milk",  
-      price: "1.30", 
+      price: 1.30, 
       type: "bottle",
       quantity: 0  
     },
     {
       name: "Beans", 
-      price: "0.73", 
+      price: 0.73, 
       type: "can",
       quantity: 0 
     }
@@ -44,5 +44,13 @@ export class DataService {
 
   getBasketContents(){ 
     return this.basket;
+  }
+
+  getTotal(){
+    let total = 0;
+    this.basket.forEach(element => {
+      total += (element.quantity * element.price);
+    });
+    return total.toFixed(2);
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,19 +9,27 @@ import { DataService } from '../data.service';
 export class ProductsComponent implements OnInit {
 
   basket: any;
+  total: any;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
     this.basket = this.data.getBasketContents(); 
+    this.getTotal();
   }
 
   addItem(id){
-    this.data.addItem(id);
+    this.data.addItem(id); 
+    this.getTotal();
   }
 
   deleteItem(id){
     this.data.deleteItem(id);
+    this.getTotal();
+  }
+
+  getTotal(){
+    this.total = this.data.getTotal();
   }
 
 }
