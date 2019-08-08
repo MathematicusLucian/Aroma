d9290133d0382f282054339cf278bc3ca046d07e
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-08 10:25:40 +0100

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BasketComponent } from './basket/basket.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'basket', component: BasketComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
