21e99db2c51b67a000746ce9abd903b04d2cdaa3
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-08 20:32:55 +0100

    show total and item count on main page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Observable } from "rxjs/Observable";
-import { Observer } from "rxjs/Observer";
+import { Observer, Observable, Subscription } from "rxjs";
 
 import { DataService } from '../services/data.service'; 
 import { BasketService } from '../services/basket.service'; 
@@ -13,16 +12,28 @@ import { Basket } from "../models/basket.model";
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent implements OnInit {
-
+export class ProductsComponent implements OnInit, OnDestroy { 
   public products: Product[]; 
+  public basket: Observable<Basket>;
+  public itemCount: number;
+  private basketSubscription: Subscription;
+  public currency: any;
 
   constructor(private data: DataService, private basketService: BasketService) { }
 
-  ngOnInit() {
-    console.log(this.data.getProducts());
-    
-    this.data.getProducts().subscribe(res => this.products = res);
+  ngOnInit(): void {
+    this.currency = "GBP";
+    this.data.getProducts().subscribe(res => this.products = res); 
+    this.basket = this.basketService.get();
+    this.basketSubscription = this.basket.subscribe((basket) => {
+      this.itemCount = basket.items.map((x) => x.quantity).reduce((p, n) => p + n, 0);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.basketSubscription) {
+      this.basketSubscription.unsubscribe();
+    }
   }
 
   public addItem(id){
