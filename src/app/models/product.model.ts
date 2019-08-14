76d1378e1e5a21e7d89365e7f5f2ae978ea2ef46
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-14 18:30:16 +0100

    update products

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,12 +1,14 @@
 export class Product {
   public id: string;
   public name: string;
+  public desc: string;
   public grouping: string;
   public price: number; 
 
   public updateFrom(src: Product): void {
     this.id = src.id;
     this.name = src.name;
+    this.desc = src.desc;
     this.grouping = src.grouping;
     this.price = src.price;
   }
